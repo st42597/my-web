225b83f210c7f2934e9b3165a0ba7474d56d4ece
st42597 <st42597@gmail.com>
2025-01-19 20:40:33 +0900

    feat: add footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <ul>
+        <li> Willki.dev</li>
+        <li>
+          © 2025 조성현 <a href="https://github.com/st42597/my-web">my-web</a>{" "}
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
