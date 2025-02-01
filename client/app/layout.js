20910b248b8465b55e468baadf8a484c936ddd5c
st42597 <st42597@gmail.com>
2025-02-01 17:08:34 +0900

    refactor: erase unused font style

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,18 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "WillKi.dev",
@@ -22,9 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Header />
         <main className="content-container">{children}</main>
         <Footer />
