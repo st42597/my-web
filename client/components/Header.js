6f7582fbf14d92680d54731a03f8fd14cb3eebfc
st42597 <st42597@gmail.com>
2025-02-06 20:56:39 +0900

    style: add header text hover color

--- client/components/Header.js
+++ client/components/Header.js
@@ -6,17 +6,22 @@ function Header() {
   return (
     <header className={styles.headerContainer}>
       <nav className={`${styles.headerNav} ${styles.sb}`}>
-        <Link href="/" className={styles.headerTitle}>
+        <Link href="/" className={`${styles.headerTitle} ${styles.headerText}`}>
           WillKi.dev
         </Link>
         <div className={styles.sb}>
-          <Link href="/posts" className={styles.mr24}>
+          <Link href="/posts" className={`${styles.headerText} ${styles.mr24}`}>
             Post
           </Link>
-          <Link href="/comment" className={styles.mr24}>
+          <Link
+            href="/comment"
+            className={`${styles.headerText} ${styles.mr24}`}
+          >
             Comment
           </Link>
-          <Link href="/about">About</Link>
+          <Link href="/about" className={styles.headerText}>
+            About
+          </Link>
         </div>
       </nav>
     </header>
