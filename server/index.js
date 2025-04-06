86f6c7055fe882e2315858e3b4d9e8bdaaae629b
st42597 <st42597@gmail.com>
2025-04-06 16:31:15 +0900

    refactor: add server routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const { Pool } = require("pg");
-const bcrypt = require("bcryptjs");
-const saltRounds = 10;
-require("dotenv").config();
 
 const app = express();
 const port = 5000;
@@ -15,73 +12,12 @@ const pool = new Pool({
   port: 5432,
 });
 
+const commentRouter = require("./routes/comments");
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/comments", async (req, res) => {
-  const { currentPage, itemsPerPage } = req.query;
-  try {
-    const result = await pool.query(
-      "SELECT id, name, comment, created_at FROM comments ORDER BY id DESC"
-    );
-    const totalPages = Math.ceil(result.rows.length / itemsPerPage);
-    const startIndex = currentPage * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const currentItems = result.rows.slice(startIndex, endIndex);
-
-    const data = {
-      totalPages: totalPages,
-      currentItems: currentItems,
-    };
-
-    res.json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error retrieving comment");
-  }
-});
-
-app.post("/comments", async (req, res) => {
-  const { name, password, comment } = req.body;
-  const encryptedPassword = await bcrypt.hash(password, saltRounds);
-  try {
-    const result = await pool.query(
-      "INSERT INTO comments (name, password, comment) VALUES ($1, $2, $3) RETURNING *",
-      [name, encryptedPassword, comment]
-    );
-    res.status(201).json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error adding comment");
-  }
-});
+app.use("/comments", commentRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
-});
-
-app.delete("/comments/:id", async (req, res) => {
-  const { id } = req.params;
-  const { password } = req.body;
-  const supervisor = process.env.ADMIN_PASSWORD;
-  try {
-    const result = await pool.query("SELECT * FROM comments WHERE id = $1", [
-      id,
-    ]);
-    if (
-      (await bcrypt.compare(password, result.rows[0].password)) ||
-      password === supervisor
-    ) {
-      await pool.query("DELETE FROM comments WHERE id = $1", [id]);
-      res.status(204).send();
-    } else {
-      res.status(401).send("Password incorrect");
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error deleting comment");
-  }
 });
---
+++ server/routes/comments.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+const { Pool } = require("pg");
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
+require("dotenv").config();
+
+const pool = new Pool({
+  user: "sh",
+  host: "db",
+  database: "mydb",
+  password: "1234",
+  port: 5432,
+});
+
+router.get("/", async (req, res) => {
+  const { currentPage, itemsPerPage } = req.query;
+  try {
+    const result = await pool.query(
+      "SELECT id, name, comment, created_at FROM comments ORDER BY id DESC"
+    );
+    const totalPages = Math.ceil(result.rows.length / itemsPerPage);
+    const startIndex = currentPage * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentItems = result.rows.slice(startIndex, endIndex);
+
+    const data = {
+      totalPages: totalPages,
+      currentItems: currentItems,
+    };
+
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error retrieving comment");
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, password, comment } = req.body;
+  const encryptedPassword = await bcrypt.hash(password, saltRounds);
+  try {
+    const result = await pool.query(
+      "INSERT INTO comments (name, password, comment) VALUES ($1, $2, $3) RETURNING *",
+      [name, encryptedPassword, comment]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error adding comment");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { password } = req.body;
+  const supervisor = process.env.ADMIN_PASSWORD;
+  try {
+    const result = await pool.query("SELECT * FROM comments WHERE id = $1", [
+      id,
+    ]);
+    if (
+      (await bcrypt.compare(password, result.rows[0].password)) ||
+      password === supervisor
+    ) {
+      await pool.query("DELETE FROM comments WHERE id = $1", [id]);
+      res.status(204).send();
+    } else {
+      res.status(401).send("Password incorrect");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting comment");
+  }
+});
+
+module.exports = router;
